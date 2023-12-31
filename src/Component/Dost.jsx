971c1b67f6a096d26a6d5844b9c103418949283f
Arj09 <arjunkushwah100199@gmail.com>
2024-01-01 00:18:55 +0530

    add

--- src/Component/Dost.jsx
+++ src/Component/Dost.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Dost = ()=>{
     return(
-        <div className=" flex flex-row justify-center bg-red-600 text-white mx-auto my-4">
-            <text>Harami dosto 2024 me sudar jana</text>
+        <div className=" flex flex-row justify-center bg-red-600 text-white mx-auto my-4 py-4">
+            <text>Ja be lawde apna kaam kar</text>
         </div>
     )
 }
